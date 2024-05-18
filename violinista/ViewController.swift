abbdaf5e79dfacd6d10a39f9c949a25c81720110
Srinidhi Sasidharan <srinidhisasidharan99@gmail.com>
2024-05-18 13:33:08 -0400

    add gitignore

---
+++ violinista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  violinista
+//
+//  Created by Srinidhi Sasidharan on 5/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
